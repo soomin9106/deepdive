81795679b462d7079abfde5923a54b751d23f9a3
soomin9106 <soomin9106@naver.com>
2024-04-24 09:12:05 +0900

    Fix Error

--- deepdive/app/components/render/Process.tsx
+++ deepdive/app/components/render/Process.tsx
@@ -9,12 +9,12 @@ const Process = () => {
             </div>
             <span>When Click Heart Icon...</span>
             <span>1. setState from Second component is called</span>
-            <span>2. put Second's re-rendering process in queue</span>
+            <span>2. put Second component re-rendering process in queue</span>
             <span>3. inspect rendering path</span>
             <span>4. There is no changes in First component</span>
             <span>5. Re-render Second component</span>
             <span>6. In previous step, Second component returns Third component</span>
-            <span>7. counter, Third component's props is updated. So, update Third component</span>
+            <span>7. counter, Third component props is updated. So, update Third component</span>
             <span>8. In previous step, Third component returns Fourth component</span>
             <span>9. There is no changes in Fourth component. However, since Third component is re-rendered, Fourth component should be re-rendered.
                 <br/> (Fourth component is in Third component.)
